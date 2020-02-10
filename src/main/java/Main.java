94ea4af13f7352cabe90450eb2af31bf415d16aa
Dyanek <nathanael.omnes@gmail.com>
2020-02-10 13:12:47 +0100

    Added maven to project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-public class MQTTSubscriber implements MqttCallback {
+public class Main implements MqttCallback {
 
     private static final String SERVER_URI = "tcp://127.0.0.1";
     private static final String TOPIC_NAME = "topic";
@@ -18,22 +18,20 @@ public class MQTTSubscriber implements MqttCallback {
 
         try {
             client = new MqttClient(SERVER_URI, MqttClient.generateClientId());
-            client.setCallback(new MQTTSubscriber());
+            client.setCallback(new Main());
             client.connect();
             client.subscribe(TOPIC_NAME);
         } catch (MqttException ex) {
-            Logger.getLogger(MQTTSubscriber.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    @Override
     public void connectionLost(Throwable cause) {
         System.out.println("-------------------------------------------------");
         System.out.println("Connection lost");
         System.out.println("-------------------------------------------------");
     }
 
-    @Override
     public void messageArrived(String topic, MqttMessage message) {
         System.out.println("-------------------------------------------------");
         System.out.println("| Topic:" + topic);
@@ -41,7 +39,6 @@ public class MQTTSubscriber implements MqttCallback {
         System.out.println("-------------------------------------------------");
     }
 
-    @Override
     public void deliveryComplete(IMqttDeliveryToken imdt) {
         System.out.println("-------------------------------------------------");
         System.out.println("Delivery complete");

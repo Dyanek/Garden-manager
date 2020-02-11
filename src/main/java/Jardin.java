257f02ef21e0a90c1482226acd1296e35030f35e
Shuyuan <779455514@qq.com>
2020-02-11 21:26:38 +0100

    pas mal :)

--- src/main/java/Jardin.java
+++ src/main/java/Jardin.java
@@ -2,22 +2,23 @@ import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.util.Scanner;
 
 public class Jardin extends JFrame {
 
     private int temperature = 26;
-    private int humidite1 = 21;
-    private int humidite2 = 22;
-    private int humidite3 = 23;
+    private int humidity1 = 21;
+    private int humidity2 = 22;
+    private int humidity3 = 23;
 
     JFrame jFrame;
-    JPanel jPanel, pageAcceuil_panel_1, pageAcceuil_panel_2, pageAcceuil_panel_3, pageAccueil_info;
-    JLabel label2, label3, labelTemperature, labelAide;
+    JPanel jPanel, panelWelcome1, panelWelcome2, panelWelcome3, panelWelcomeInfo;
+    JLabel label2, label3, labelTemperature, labelHelp;
 
     JPanel panelWater, panelHelp;
-    JLabel jLabelWaterTitle, jLabelWaterSubTitle, jLabelHumidite1, jLabelHumidite2, jLabelHumidite3, jLabelTitle, jLabelHelp1, jLabelHelp2, jLabelHelp3;
+    JLabel jLabelWaterTitle, jLabelWaterSubTitle, jLabelHumidity1, jLabelHumidity2, jLabelHumidity3, jLabelHelpTitle, jLabelHelp1, jLabelHelp2, jLabelHelp3;
 
     public void laucheInterface(){
 
@@ -34,45 +35,61 @@ public class Jardin extends JFrame {
         jFrame.setSize(width, height);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // -- jPanel : 4 Components --
-        this.pageAcceuil_panel_1 = new WebcamPanel(Webcam.getDefault());
-
-        this.pageAcceuil_panel_2 = new JPanel();
+        // -- panelWelcome1 : 4 Components --
+        // camera 1
+        this.panelWelcome1 = new WebcamPanel(Webcam.getDefault());
+        this.panelWelcome1.setBorder(new LineBorder(Color.gray));
+        // camera 2
+        this.panelWelcome2 = new JPanel();
+        this.panelWelcome2.setBorder(new LineBorder(Color.gray));
         label2 = new JLabel("Caméra 2");
-        pageAcceuil_panel_2.add(label2);
-
-        this.pageAcceuil_panel_3 = new JPanel();
+        panelWelcome2.add(label2);
+        // camera 3
+        this.panelWelcome3 = new JPanel();
+        this.panelWelcome3.setBorder(new LineBorder(Color.gray));
         label3 = new JLabel("Caméra 3");
-        pageAcceuil_panel_3.add(label3);
-
-        this.pageAccueil_info = new JPanel();
-        labelTemperature = new JLabel("Temperature : " + temperature + " °C");
+        panelWelcome3.add(label3);
+        // panelWelcomeInfo
+        this.panelWelcomeInfo = new JPanel();
+        this.panelWelcomeInfo.setBorder(new LineBorder(Color.gray));
+        labelTemperature = new JLabel("Temperature : " + temperature + " °C", SwingConstants.CENTER);
+        labelHelp = new JLabel("Dites \"Aide\" pour afficher la liste des commandes disponibles", SwingConstants.CENTER);
         labelTemperature.setFont(new Font( "Serif", Font.BOLD, 20));
-        pageAccueil_info.add(labelTemperature);
-        labelAide = new JLabel("Dites \"Aide\" pour afficher la liste \n" +
-                "des commandes disponibles");
-        pageAccueil_info.add(labelAide);
+        panelWelcomeInfo.add(labelTemperature);
+        panelWelcomeInfo.add(labelHelp);
+        panelWelcomeInfo.setLayout(new GridLayout(2, 0));
 
         // add components
         jPanel = new JPanel();
 
-        // Loading Page d'acceuil
+        // loading Page d'acceuil
         activeAcceuil();
 
         jFrame.add(jPanel);
 
         // Panel Help
         panelHelp = new JPanel();
-        jLabelTitle = new JLabel("--- Aide ---");
-        jLabelTitle.setFont(new Font( "Serif", Font.BOLD, 15));
-        jLabelHelp1 = new JLabel("1. temperature [chiffre]");
-        jLabelHelp2 = new JLabel("2. arroser tout");
-        jLabelHelp3 = new JLabel("3. acceuil");
-        panelHelp.add(jLabelTitle);
+        jLabelHelpTitle = new JLabel("------ Aide ------", SwingConstants.CENTER);
+        jLabelHelpTitle.setFont(new Font( "Serif", Font.BOLD, 20));
+        jLabelHelp1 = new JLabel("1. temperature [chiffre]", SwingConstants.CENTER);
+        jLabelHelp1.setFont(new Font( "Serif", Font.BOLD, 15));
+        jLabelHelp2 = new JLabel("2. arroser tout", SwingConstants.CENTER);
+        jLabelHelp2.setFont(new Font( "Serif", Font.BOLD, 15));
+        jLabelHelp3 = new JLabel("3. acceuil", SwingConstants.CENTER);
+        jLabelHelp3.setFont(new Font( "Serif", Font.BOLD, 15));
+        panelHelp.add(jLabelHelpTitle);
         panelHelp.add(jLabelHelp1);
         panelHelp.add(jLabelHelp2);
         panelHelp.add(jLabelHelp3);
         panelHelp.setLayout(new GridLayout(5, 0));
+
+        // Panel Water
+        panelWater = new JPanel();
+        panelWater.setLayout(new GridLayout(5,0));
+        jLabelWaterSubTitle = new JLabel("Taux de humidite : \n");
+        jLabelHumidity1 = new JLabel("    Etage 1 : " + humidity1 + "%");
+        jLabelHumidity2 = new JLabel("    Etage 2 : " + humidity2 + "%");
+        jLabelHumidity3 = new JLabel("    Etage 3 : " + humidity3 + "%");
 
 
         // set window visible and center
@@ -87,54 +104,84 @@ public class Jardin extends JFrame {
         System.out.println(">>> temperature uploaded : " + temperature);
     }
 
+    public void uploadHumidity1(int humidity1){
+        this.humidity1 = humidity1;
+        jLabelHumidity1.setText("    Etage 1 : " + humidity1 + "%");
+        System.out.println(">>> humidity1 uploaded : " + humidity1);
+    }
+
+    public void uploadHumidity2(int humidity2){
+        this.humidity2 = humidity2;
+        jLabelHumidity2.setText("    Etage 2 : " + humidity2 + "%");
+        System.out.println(">>> humidity2 uploaded : " + humidity2);
+    }
+
+    public void uploadHumidity3(int humidity3){
+        this.humidity3 = humidity3;
+        jLabelHumidity3.setText("    Etage 3 : " + humidity3 + "%");
+        System.out.println(">>> humidity3 uploaded : " + humidity3);
+    }
+
     public void waterAll(){
         this.jPanel.removeAll();
         this.jPanel.setLayout(new GridLayout(2,2));
-        this.jPanel.add(pageAcceuil_panel_1);
-        this.jPanel.add(pageAcceuil_panel_2);
-        this.jPanel.add(pageAcceuil_panel_3);
+        this.jPanel.add(panelWelcome1);
+        this.jPanel.add(panelWelcome2);
+        this.jPanel.add(panelWelcome3);
 
-        panelWater = new JPanel();
-        jLabelWaterTitle = new JLabel("Arrosage tout les étages en cours\n");
+        panelWater.removeAll();
+        jLabelWaterTitle = new JLabel("Arrosage tout les étages en cours");
         jLabelWaterTitle.setFont(new Font( "Serif", Font.BOLD, 20));
-        jLabelWaterSubTitle = new JLabel("Taux de humidite : \n");
-        jLabelHumidite1 = new JLabel("    Etage 1 : " + humidite1 + "%");
-        jLabelHumidite2 = new JLabel("    Etage 2 : " + humidite2 + "%");
-        jLabelHumidite3 = new JLabel("    Etage 3 : " + humidite3 + "%");
 
         panelWater.add(jLabelWaterTitle);
         panelWater.add(jLabelWaterSubTitle);
-        panelWater.add(jLabelHumidite1);
-        panelWater.add(jLabelHumidite2);
-        panelWater.add(jLabelHumidite3);
+        panelWater.add(jLabelHumidity1);
+        panelWater.add(jLabelHumidity2);
+        panelWater.add(jLabelHumidity3);
         panelWater.setLayout(new GridLayout(5,0));
         this.jPanel.add(panelWater);
         this.jFrame.validate();
     }
     public void waterFirst(){
+
+        panelWater.removeAll();
+        jLabelWaterTitle = new JLabel(" Arrosage de premier étage en cours");
+        jLabelWaterTitle.setFont(new Font( "Serif", Font.BOLD, 20));
+        panelWater.add(jLabelWaterTitle);
+        panelWater.add(jLabelWaterSubTitle);
+        panelWater.add(jLabelHumidity1);
+
         this.jPanel.removeAll();
         this.jPanel.setLayout(new GridLayout(0,2));
-        this.jPanel.add(pageAcceuil_panel_1);
-        panelWater.removeAll();
-        panelWater.add(jLabelHumidite1);
+        this.jPanel.add(panelWelcome1);
         this.jPanel.add(panelWater);
         this.jFrame.validate();
     }
     public void waterSecond(){
+        panelWater.removeAll();
+        jLabelWaterTitle = new JLabel(" Arrosage de deuxieme étage en cours");
+        jLabelWaterTitle.setFont(new Font( "Serif", Font.BOLD, 20));
+        panelWater.add(jLabelWaterTitle);
+        panelWater.add(jLabelWaterSubTitle);
+        panelWater.add(jLabelHumidity2);
+
         this.jPanel.removeAll();
         this.jPanel.setLayout(new GridLayout(0,2));
-        this.jPanel.add(pageAcceuil_panel_2);
-        panelWater.removeAll();
-        panelWater.add(jLabelHumidite2);
+        this.jPanel.add(panelWelcome2);
         this.jPanel.add(panelWater);
         this.jFrame.validate();
     }
     public void waterThird(){
+        panelWater.removeAll();
+        jLabelWaterTitle = new JLabel(" Arrosage de troisieme étage en cours");
+        jLabelWaterTitle.setFont(new Font( "Serif", Font.BOLD, 20));
+        panelWater.add(jLabelWaterTitle);
+        panelWater.add(jLabelWaterSubTitle);
+        panelWater.add(jLabelHumidity3);
+
         this.jPanel.removeAll();
         this.jPanel.setLayout(new GridLayout(0,2));
-        this.jPanel.add(pageAcceuil_panel_3);
-        panelWater.removeAll();
-        panelWater.add(jLabelHumidite3);
+        this.jPanel.add(panelWelcome3);
         this.jPanel.add(panelWater);
         this.jFrame.validate();
     }
@@ -142,35 +189,31 @@ public class Jardin extends JFrame {
 
     public void activeAcceuil(){
         this.jPanel.removeAll();
-        jPanel.add(pageAcceuil_panel_1);
-        jPanel.add(pageAcceuil_panel_2);
-        jPanel.add(pageAcceuil_panel_3);
-        jPanel.add(pageAccueil_info);
+        jPanel.add(panelWelcome1);
+        jPanel.add(panelWelcome2);
+        jPanel.add(panelWelcome3);
+        jPanel.add(panelWelcomeInfo);
         jPanel.setLayout(new GridLayout(2,2));
         this.jFrame.validate();
     }
 
     public void launchHelp(){
-        jPanel.removeAll();
-        jPanel.add(pageAcceuil_panel_1);
-        jPanel.add(pageAcceuil_panel_2);
-        jPanel.add(pageAcceuil_panel_3);
+        this.jPanel.removeAll();
         jPanel.add(panelHelp);
-        jPanel.setLayout(new GridLayout(0,3));
+        jPanel.setLayout(new GridLayout(1,0));
         jFrame.validate();
     }
 
-    // -- TEST --
-
+    // -- TEST a l'aide console --
     public void getCommande(){
         Scanner scanner = new Scanner( System.in );
         String input = "";
         while(!input.equals("exit")){
-            System.out.println("Dit-moi, qu'est-ce que tu veux hen ???(\"help\" if u need help O.O)");
+            System.out.print("Dit-moi, qu'est-ce que tu veux hen ???(\"help\" if u need help O.O) : ");
             input = scanner.nextLine();
             traiterCommande(input);
         }
-        System.out.println("Bye ^^");
+        System.out.println("Bye, a plus ^^");
         System.exit(0);
     }
 
@@ -179,6 +222,12 @@ public class Jardin extends JFrame {
 
         if (list[0].toLowerCase().equals("temperature"))
             uploadTemperature(Integer.valueOf(list[1]));
+        else if(list[0].toLowerCase().equals("humidity1"))
+            uploadHumidity1(Integer.valueOf(list[1]));
+        else if(list[0].toLowerCase().equals("humidity2"))
+            uploadHumidity2(Integer.valueOf(list[1]));
+        else if(list[0].toLowerCase().equals("humidity3"))
+            uploadHumidity3(Integer.valueOf(list[1]));
         else if(list[0].toLowerCase().equals("aide")){
             launchHelp();
             System.out.println("Page de l'aide affiche...");

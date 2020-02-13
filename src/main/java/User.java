818a3351709db4c15dc6b733bacf32ea522ac741
Shuyuan <779455514@qq.com>
2020-02-13 20:35:11 +0100

    classe user terminer

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -21,12 +21,24 @@ class User {
         return executedLightingsCount;
     }
 
+    void increaseExecutedCommqndsCount() {
+        executedCommandsCount++;
+    }
+
+    void increaseExecutedWateringsCount() {
+        increaseExecutedCommqndsCount();
+        executedWateringsCount++;
+    }
+
+    void increaseExecutedLightingsCount() {
+        increaseExecutedCommqndsCount();
+        executedLightingsCount++;
+    }
+
     User(String userName) {
         this.userName = userName;
         this.executedCommandsCount = 0;
         this.executedWateringsCount = 0;
         this.executedLightingsCount = 0;
     }
-
-
 }
